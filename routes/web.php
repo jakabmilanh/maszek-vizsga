e38ce03eb042565dc665c44a2844b1332d8750e8
jakabmilanh <77169556+jakabmilanh@users.noreply.github.com>
2025-01-24 22:40:41 +0100

    dd

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/home#contact', [HomeController::class, 'indexkapcsolat'])->name('home#kapcsolat');
-
 Route::get('/', function () {
     return view('home');
-})->middleware(['auth', 'verified'])->name('dashboard');
+});
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home#contact', [HomeController::class, 'indexkapcsolat'])->name('home#kapcsolat');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
