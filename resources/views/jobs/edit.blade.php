88fdfad66fbcbd7cc9b0b98929a083922fe6c7b4
jakabmilanh <77169556+jakabmilanh@users.noreply.github.com>
2025-04-27 21:26:15 +0200

    asd
    asd

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['../../resources/css/app.css', 'resources/js/app.js',])
+            @vite(['resources/css/app.css', 'resources/js/app.js',])
         @endif
     </head>
     <body data-bs-spy="scroll" data-bs-target="#navbar" data-bs-offset="71">
